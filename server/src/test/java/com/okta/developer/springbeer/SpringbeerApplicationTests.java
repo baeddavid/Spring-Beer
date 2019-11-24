d097a7a3b64da9a8663d67d9dcd1adbc079ceb59
David Bee <baeddavid@gmail.com>
2019-11-24 14:27:37 -0600

    initial commit

---
+++ server/src/test/java/com/okta/developer/springbeer/SpringbeerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.okta.developer.springbeer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbeerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
